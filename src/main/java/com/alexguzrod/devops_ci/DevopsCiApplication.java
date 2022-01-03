a08487c878863712ee8194bf9c9ebbf4de1cd198
Alexander Guzmán R <alex_guzman_7@hotmail.com>
2022-01-03 10:09:06 -0500

    creating project

---
+++ src/main/java/com/alexguzrod/devops_ci/DevopsCiApplication.java
@@ -0,0 +1,13 @@
+package com.alexguzrod.devops_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexguzrod/devops_ci/DevopsCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexguzrod.devops_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
